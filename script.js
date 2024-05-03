58ef90a6eccab894e2778e43aa503a46ffd99456
Adam Troxell <adamtxl@hotmail.com>
2024-05-03 11:38:56 -0500

    formatting

--- script.js
+++ script.js
@@ -1,42 +1,39 @@
 onReady();
 
 function onReady() {
-  console.log('Javascript is working!');
+	console.log('Javascript is working!');
 }
-const iceButton = document.getElementById("ice-button");
-const fireButton = document.getElementById("fire-button");
-let affirmationTable = document.getElementById("affirmation-table")
-let pageBody = document.getElementById("emojisHere");
+const iceButton = document.getElementById('ice-button');
+const fireButton = document.getElementById('fire-button');
+let affirmationTable = document.getElementById('affirmation-table');
+let pageBody = document.getElementById('emojisHere');
 
-let affirmationInput = document.getElementById("affirmation-affirmation");
-let authorInput = document.getElementById("affirmation-author");
+let affirmationInput = document.getElementById('affirmation-affirmation');
+let authorInput = document.getElementById('affirmation-author');
 
-function createFire (event){
-  console.log('fire button clicked')
-pageBody.innerHTML += '<p>🔥</p>'
+function createFire(event) {
+	console.log('fire button clicked');
+	pageBody.innerHTML += '<p>🔥</p>';
 }
 
-function createIce (event){
- console.log('ice button clicked')  
-  pageBody.innerHTML += `<p>❄️</p>`
-  
-  
+function createIce(event) {
+	console.log('ice button clicked');
+	pageBody.innerHTML += `<p>❄️</p>`;
 }
 // debugger;
-function addAffirmations (event){
-  event.preventDefault();
-  console.log('submitted')
-  //add input values as table items
-  affirmationTable.innerHTML += 
-  `<tr> 
+function addAffirmations(event) {
+	event.preventDefault();
+	console.log('submitted');
+	//add input values as table items
+	affirmationTable.innerHTML += `<tr> 
   <td>${affirmationInput.value}</td>
   <td>${authorInput.value}</td>
   <td> <button class = "delete-btn" onClick="deleteTask(event)">❌</button> </td>
-  </tr>`
-  affirmationInput.value = '';
-  authorInput.value = '';
+  </tr>`;
+	affirmationInput.value = '';
+	authorInput.value = '';
 }
 
 function deleteTask(event) {
-  event.target.parentElement.parentElement.remove();
+	event.target.parentElement.parentElement.remove();
 }
