66895628251857e6ff53c13898d2386751bc862d
Adam Troxell <adamtxl@hotmail.com>
2024-05-03 10:53:38 -0500

    Fire and Ice buttons correctly appending emojis to DOM

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ onReady();
 function onReady() {
   console.log('Javascript is working!');
 }
+let iceButton = document.getElementById("ice-button")
+let fireButton = document.getElementById("fire-button");
+let pageBody = document.getElementById("emojisHere")
+
+function createFire (event){
+  console.log('fire button clicked')
+pageBody.innerHTML += '<p>🔥</p>'
+}
+
+function createIce (event){
+ console.log('ice button clicked')  
+  pageBody.innerHTML += `<p>❄️</p>`
+  
+  
+}
